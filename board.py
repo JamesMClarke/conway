117120eea3e7e3e2853b8ceb0b511fbcac725173
james <james@pop-os.localdomain>
2021-06-10 22:36:58 +0100

    changed name of run

---
+++ board.py
@@ -0,0 +1,27 @@
+from square import Square
+import random
+
+def main():
+    board = Board()
+    board.print_grid()
+
+class Board:
+    width = 8
+    length = 8
+
+    def __init__(self):
+        self.grid = [[Square(bool(random.getrandbits(1))) for j in range(self.width)] for i in range(self.length)]
+
+
+    def print_grid(self):
+        for x in self.grid:
+            for y in x:
+                if(y.get_is_alive()):
+                    print("X", end ="|")
+                else:
+                    print(" ", end ="|")
+            print("")
+            print("-".join(["-"] * self.width))
+
+if __name__ == "__main__":
+    main()
