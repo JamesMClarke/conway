f6ff54e57e3aaf0e502c0b9e792b476331210d4f
james <james@pop-os.localdomain>
2021-06-10 21:31:53 +0100

    basics for sqaure added

---
+++ square.py
@@ -0,0 +1,12 @@
+class Square:
+    def __init__(self,  is_alive:
+        self.is_alive = is_alive:
+    
+def get_is_alive():
+    return self.is_alive
+
+def kill():
+    self.is_alive = False
+
+def revive():
+    self.is_alive = True 
