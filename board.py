05ca44ea720b48f6824f307347cf4b63ce1c63af
root <root@pop-os.localdomain>
2021-06-11 17:28:18 +0100

    added a onclick listner to rectangels

--- board.py
+++ board.py
@@ -25,14 +25,45 @@ class Board:
             print("")
             print("-".join(["-"] * self.width))
 
+    def change_cell_state(self):from square import Square
+import random
+from tkinter import *
 
+def main():
+    board = Board()
+    board.print_grid()
+    board.grid_gui()
+
+class Board:
+    width = 8
+    length = 8
+
+    def __init__(self):
+        self.grid = [[Square(bool(random.getrandbits(1))) for j in range(self.width)] for i in range(self.length)]
+
+
+    def print_grid(self):
+        for x in self.grid:
+            for y in x:
+                if(y.get_is_alive()):
+                    print("X", end ="|")
+                else:
+                    print("D", end ="|")
+            print("")
+            print("-".join(["-"] * self.width))
+
+    
+    
     def grid_gui(self):
-        #ToDo adjust rect size 
+        #ToDo put  in a frame
         #creates tkinter gui grid on a canvas
 
     
         root = Tk()
-        self.grid_canvas = Canvas(root, width=800, height=800)
+        root.title("Conways Game of Life")
+
+
+        self.canvas = Canvas(root, width=800, height=800)
         x_coord = 0
         y_coord = 0 
 
@@ -45,21 +76,31 @@ class Board:
 
                 if(y.get_is_alive()):
                     #coordinates squares are overwiring one another
-                    self.grid_canvas.create_rectangle(x_coord, y_coord, x_coord+rect_size, y_coord+rect_size, fill = "Green", outline = 'black',width=1)
+                    self.canvas.create_rectangle(x_coord, y_coord, x_coord+rect_size, y_coord+rect_size, fill = "Green", outline = 'black',width=1, tags ="setStateButton")
+                    self.canvas.tag_bind("setStateButton","<Button-1>",self.change_cell_state)
+
                 else:
-                    self.grid_canvas.create_rectangle(x_coord, y_coord, x_coord+rect_size, y_coord+rect_size, fill = "red", outline = 'black',width=1)
+                    self.canvas.create_rectangle(x_coord, y_coord, x_coord+rect_size, y_coord+rect_size, fill = "red", outline = 'black',width=1,tags ="setStateButton")
+                    self.canvas.tag_bind("setStateButton","<Button-1>",self.change_cell_state)
+
                 x_coord = x_coord + rect_size
 
+                    
 
                 #if x_coord is >= than grid width * rect width, increments y_coord by itself plus rect width. resets x_coord coord
                 if x_coord >= self.width*rect_size:
                     y_coord = y_coord+rect_size
                     x_coord = 0 
-                self.grid_canvas.pack()
+                self.canvas.pack()
+
+
                 
                 
         root.mainloop()
-         
+
+    def change_cell_state(a,b):
+
+        print("A ",a,"\n","B",b)
 
 
 if __name__ == "__main__":
