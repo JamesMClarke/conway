357f13a0a9aaae790e822657651a13141381ef08
root <root@pop-os.localdomain>
2021-06-11 15:01:15 +0100

    added basic tkinter gui

--- board.py
+++ board.py
@@ -1,9 +1,11 @@
 from square import Square
 import random
+from tkinter import *
 
 def main():
     board = Board()
     board.print_grid()
+    board.grid_gui()
 
 class Board:
     width = 8
@@ -19,9 +21,43 @@ class Board:
                 if(y.get_is_alive()):
                     print("X", end ="|")
                 else:
-                    print(" ", end ="|")
+                    print("D", end ="|")
             print("")
             print("-".join(["-"] * self.width))
+
+
+    def grid_gui(self):
+        #ToDo adjust rect size 
+        #creates tkinter gui grid on a canvas
+
+    
+        root = Tk()
+        self.grid_canvas = Canvas(root, width=800, height=800)
+        x_coord = 0
+        y_coord = 0 
+
+        for x in self.grid:
+            for y in x:
+                # start new line if greater than width
+
+                if(y.get_is_alive()):
+                    #coordinates squares are overwiring one another
+                    self.grid_canvas.create_rectangle(x_coord, y_coord, x_coord+25, y_coord+25, fill = "Green", outline = 'black',width=1)
+                else:
+                    self.grid_canvas.create_rectangle(x_coord, y_coord, x_coord+25, y_coord+25, fill = "red", outline = 'black',width=1)
+                x_coord = x_coord + 25
+
+
+                #if x_coord is >= than grid width * rect width, increments y_coord by itself plus rect width. resets x_coord coord
+                if x_coord >= self.width*25:
+                    y_coord = y_coord+25
+                    x_coord = 0 
+                self.grid_canvas.pack()
+                
+                
+        root.mainloop()
+         
+
 
 if __name__ == "__main__":
     main()
